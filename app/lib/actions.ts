f8301c13b71171c1e5b5b94a1084275187db7d61
Ovidiojcf <ojcfarias@gmail.com>
2024-10-24 20:03:44 -0300

    actions.ts update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,6 +59,7 @@ export async function createInvoice( prevState: State ,formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
     } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
         return {
         message: 'Database Error: Failed to Create Invoice.',
         };
@@ -93,6 +94,7 @@ export async function updateInvoice(id: string, prevState: State,formData: FormD
             WHERE id = ${id}
           `;
       } catch (error) {
+        console.error('Error occurred:', error)
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
    
@@ -107,6 +109,7 @@ export async function deleteInvoice(id: string) {
         revalidatePath('/dashboard/invoices');
         return { message: 'Deleted Invoice.' };
       } catch (error) {
+        console.error('Error occurred:', error)
         return { message: 'Database Error: Failed to Delete Invoice.' };
     }
 }
