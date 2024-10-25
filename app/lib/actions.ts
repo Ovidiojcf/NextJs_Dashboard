75c2ece92bd19dced932b516ba6a52976f138492
Ovidiojcf <ojcfarias@gmail.com>
2024-10-25 19:46:04 -0300

    eslint no-unsed-vars

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -52,7 +52,8 @@ export async function createInvoice( prevState: State ,formData: FormData) {
     const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-
+    
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
